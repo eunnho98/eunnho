842d74588d886943d11625090a4d305b71815fa1
eunnho98 <taemin4u@khu.ac.kr>
2024-02-29 15:07:54 +0900

    chore: update profilr

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -8,8 +8,8 @@ function Profile() {
         <Avatar size="2xl" src="img.jpg" border="4px solid black" zIndex={-1} />
         <VStack>
           <Text fontSize="xl">
-            Hello, I am an undergraduate research student at the UXC Lab in the
-            Department of Software Convergence at KyungHee University.
+            Hello, I am a masters student at the UXC Lab in the Department of
+            Software Convergence at KyungHee University.
           </Text>
         </VStack>
       </HStack>
